dae2a8f64c2c51bee1c9ff2348dd0b0f88830bf1
Jan D <jandohring@hotmail.com>
2023-11-30 16:19:54 -0500

    practice 2015

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,4 +1,27 @@
 fn main() {
+    // Day 1 2015
+
+    let mut floor = 0;
+    let mut position = 0;
+    let mut first_basement_position : Option<i32> = None;
     let input = include_str!("./input.txt");
-    println!("{}", input);
+    
+    for char in input.chars() {
+        position += 1;
+        match char {
+            '(' => {
+                floor += 1;
+            }
+            _ => {
+                floor -= 1;
+            }
+        }
+
+        if first_basement_position == None && floor == -1 {
+            first_basement_position = Some(position);
+        }
+    }
+
+    println!("Part 1: Floor is {}", floor);
+    println!("Part 2: Position is {}", first_basement_position.unwrap());
 }
